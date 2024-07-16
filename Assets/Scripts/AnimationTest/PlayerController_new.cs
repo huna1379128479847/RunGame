191887ac358329c1005e57bda8c49787ea8ee3a2
noa0610 <human.kagoshima.yamanaka@gmail.com>
2024-07-16 18:15:06 +0900

    アニメーションテスト　完了

---
+++ Assets/Scripts/AnimationTest/PlayerController_new.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_new : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpPow = 300f;
+    [SerializeField]private bool onFloor = true;
+    private bool gameover = false;
+    [SerializeField] private Rigidbody2D rbody;
+    [SerializeField] private Animator animator;
+    // アニメーションの設定
+    // SetBool"isWalk"    = 歩くアニメーション
+    // SetBool"isJump"    = ジャンプアニメーション
+    // SetBool"isFalling" = 落下中アニメーション
+    // SetBool"isLanding" = 着地アニメーション
+    // trueでアニメーションが再生される falseでアニメーションが再生されなくなる
+
+    void Start()
+    {
+        SceneChanger.instance.player = gameObject;
+        rbody = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        this.animator.SetBool("isWalk",true);
+        if (Input.GetKeyDown(KeyCode.Space) && onFloor && !gameover)
+        {
+            
+            rbody.AddForce(transform.up * jumpPow);
+            onFloor = false;
+        }
+
+        if(this.rbody.velocity.y > 0) // プレイヤーが上昇しているとき
+        {
+            this.animator.SetBool("isJump",true);
+            this.animator.SetBool("isWalk",false);
+        }
+        else
+        {
+            this.animator.SetBool("isJump",false);
+            this.animator.SetBool("isWalk",true);
+        }
+
+        if (this.rbody.velocity.y < 0) // プレイヤーが下降しているとき
+        {
+            this.animator.SetBool("isFalling",true);
+            this.animator.SetBool("inWalk",false);
+        }
+        else
+        {
+            this.animator.SetBool("isFalling", false);
+            this.animator.SetBool("isWalk",true);
+        }
+        if (this.rbody.velocity.y == 0) // プレイヤーが下降しているとき
+        {
+            StartCoroutine(Landing());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+        if (collisionGameObject.tag == "floor")
+        {
+            onFloor = true;
+            
+        }
+    }
+    // FixedUpdate �͌Œ莞�Ԃ��ƂɌĂяo�����
+    private void FixedUpdate()
+    {
+        if (!gameover)
+        {
+            rbody.velocity = new Vector2(speed, rbody.velocity.y);
+            //text.SetText($"Now Y Pow:{rbody.velocity.y}");
+        }
+    }
+
+    public void Notify_Gameover()
+    {
+        gameover = true;
+    }
+
+    IEnumerator Landing()
+    {
+        this.animator.SetBool("isLanding",true);
+        this.animator.SetBool("inWalk",false);
+        yield return new WaitForSeconds(0.1f);
+        this.animator.SetBool("isLanding",false);
+        this.animator.SetBool("isWalk",true);
+    }
+}
